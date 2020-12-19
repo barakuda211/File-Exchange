6a9a75cd4d968da8014f2efdf9daa5d2185076df
barakuda211 <stalkerchnebo@gmail.com>
2020-12-19 23:48:34 +0300

    added registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  validates :id_user, presence: true
+  # validates :id_user, presence: true
   has_many :user_files
   has_many :open_files
 end
