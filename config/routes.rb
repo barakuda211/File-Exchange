22da320ab2297852c3c676745114489cc6294f8d
barakuda211 <stalkerchnebo@gmail.com>
2020-12-31 18:54:42 +0300

    added to controller to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   #devise_for :users
   devise_for :users, controllers: {
-      sessions: 'users/sessions'
+      sessions: 'users/sessions',
+      omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
 
